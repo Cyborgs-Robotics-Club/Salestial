ee391c804ac7fd1de9453d4a970f84a90430329f
vrushal09 <mehtavrushalvm@gmail.com>
2025-01-22 00:58:25 +0530

    signup page done

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,4 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, createUserWithEmailAndPassword };
 const analytics = getAnalytics(app);
